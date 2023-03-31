bc01d2bddc49f079e46c5f74356ac79196641cfa
lcmnet <lcmnet@hotmail.com>
2023-03-31 21:57:54 +0800

    added sql server connection and api is up

---
+++ Data/Class.cs
@@ -0,0 +1,51 @@
+﻿using AutoFixture;
+using Microsoft.EntityFrameworkCore;
+
+namespace mymulticontainer.Data
+{
+
+    public class Product
+    {
+        public int ProductId { get; set; }
+        public string? Name { get; set; }
+        public ProductCategory ProductCategory { get; set; }
+        public string? Description { get; set; }
+        public decimal Price { get; set; }
+        public string? SKU { get; set; }
+        public string? Code { get; set; }
+    }
+    public enum ProductCategory
+    {
+        Clothing = 1,
+        Footware = 2,
+        Electronics = 3,
+        Household = 4
+    }
+
+    public class SalesContext : DbContext
+    {
+        public SalesContext(DbContextOptions<SalesContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Product>? Products { get; set; }
+    }
+
+    public static class Seeder
+    {
+
+        // This is purely for a demo, don't anything like this in a real application!
+        public static void Seed(this SalesContext salesContext)
+        {
+            if (!salesContext.Products.Any())
+            {
+                Fixture fixture = new Fixture();
+                fixture.Customize<Product>(product => product.Without(p => p.ProductId));
+                //--- The next two lines add 100 rows to your database
+                List<Product> products = fixture.CreateMany<Product>(100).ToList();
+                salesContext.AddRange(products);
+                salesContext.SaveChanges();
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using mymulticontainer.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,8 +9,17 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<SalesContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("SalesDb")));
 
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var salesContext = scope.ServiceProvider.GetRequiredService<SalesContext>();
+    salesContext.Database.EnsureCreated();
+    salesContext.Seed();
+}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -16,8 +28,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+// Configure the HTTP request pipeline.
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
 
 app.MapControllers();
